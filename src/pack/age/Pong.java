f3de95589230f77b1f96961ad0b4b02f6de6ab5a
unknown <Admin@KCV-INLABDEBB69.kicvi.ldsb.edu>
2014-02-07 19:50:25 -0500

    makes art

--- src/pack/age/Pong.java
+++ src/pack/age/Pong.java
@@ -8,36 +8,44 @@ import java.awt.*;
 import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.util.Timer;
+import java.util.Random;
 /**
  *
  * @author Admin
  */
-public class Pong
-{
-    JFrame frame;
-    public Pong()
-    {
-        frame = new JFrame("PongTestText");
-        frame.setSize(500, 500);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        /*JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(375, 300));
-        frame.getContentPane().add(emptyLabel , BorderLayout.CENTER);
-        frame.pack();
-        frame.setVisible(true);*/
-        
-        Graphics rect = frame.getGraphics();
-        rect.fillRect(10, 10, 10, 10);
-        rect.setColor(Color.yellow);
+public class Pong extends JPanel{
 
-        //5. Show it.
-        frame.setVisible(true);
+    // Create a constructor method
+    public Pong(){
+        super();
     }
 
-    public static void main(String[] args)
-    {
-        new Pong();
+     public void paintComponent(Graphics g){
+        Random rand = new Random();
+        g.setColor(Color.getHSBColor(0, 255, 0));
+        g.fillRect(0, 0, 500, 500);
+        
+        
+        g.setColor(Color.getHSBColor(rand.nextInt(256), rand.nextInt(128)+128, rand.nextInt(256)));
+        g.drawLine(0,0,500,500); // Draw a line from (10,10) to (150,150)
+        for(int i = 0; i < rand.nextInt(30)+10; i++){
+            g.setColor(Color.getHSBColor(rand.nextInt(256), rand.nextInt(128)+128, rand.nextInt(256)));
+            g.fillRect(rand.nextInt(500), rand.nextInt(500), rand.nextInt(50), rand.nextInt(50));
+        }
+        
+    }
+
+    public static void main(String arg[]){
+        JFrame frame = new JFrame("BasicPanel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        Timer timer = new Timer();
+        
+        
+        Pong panel = new Pong();
+        frame.setContentPane(panel);          
+        frame.setVisible(true);                   
     }
 }
