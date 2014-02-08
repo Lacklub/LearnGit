e51b4a9de1f30c240064e32077e80cf73041412b
unknown <Admin@KCV-INLABDEBB69.kicvi.ldsb.edu>
2014-02-07 20:18:05 -0500

    change over time was added

--- src/pack/age/Pong.java
+++ src/pack/age/Pong.java
@@ -9,25 +9,34 @@ import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.util.Timer;
-import java.util.Random;
-/**
+import java.util.*;
+
+/*
  *
  * @author Admin
  */
 public class Pong extends JPanel{
 
     // Create a constructor method
-    public Pong(){
+    public Pong(JFrame frame){
         super();
+        Timer timer = new Timer();
+        final JFrame f = frame;
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+              paintComponent(f.getGraphics());
+            }
+         }, 100, 100);
     }
 
-     public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g){
+        
         Random rand = new Random();
         g.setColor(Color.getHSBColor(0, 255, 0));
         g.fillRect(0, 0, 500, 500);
-        
-        
+
+
         g.setColor(Color.getHSBColor(rand.nextInt(256), rand.nextInt(128)+128, rand.nextInt(256)));
         g.drawLine(0,0,500,500); // Draw a line from (10,10) to (150,150)
         for(int i = 0; i < rand.nextInt(30)+10; i++){
@@ -41,11 +50,11 @@ public class Pong extends JPanel{
         JFrame frame = new JFrame("BasicPanel");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
-        Timer timer = new Timer();
         
         
-        Pong panel = new Pong();
+        
+        Pong panel = new Pong(frame);
         frame.setContentPane(panel);          
-        frame.setVisible(true);                   
+        frame.setVisible(true);
     }
 }
