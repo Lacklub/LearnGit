95d0f2b27863d8b993c9ec0f0eb4d52c0b9b83cd
unknown <Admin@KCV-INLABDEBB69.kicvi.ldsb.edu>
2014-02-07 21:22:06 -0500

    added slight wiggling to the rectangles

--- src/pack/age/Pong.java
+++ src/pack/age/Pong.java
@@ -22,26 +22,45 @@ public class Pong extends JPanel{
         super();
         Timer timer = new Timer();
         final JFrame f = frame;
+        
+        final Vector<Rectangle> rects = new Vector<Rectangle>();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-              paintComponent(f.getGraphics());
+              f.removeAll();
+              paintComponent(f.getGraphics(), rects);
+              f.update(f.getGraphics());
             }
-         }, 100, 100);
+         }, 20, 20);
     }
-
-    public void paintComponent(Graphics g){
+    
+    public void paintComponent(Graphics g, Vector<Rectangle> rects){
         
         Random rand = new Random();
         g.setColor(Color.getHSBColor(0, 255, 0));
-        g.fillRect(0, 0, 500, 500);
-
-
-        g.setColor(Color.getHSBColor(rand.nextInt(256), rand.nextInt(128)+128, rand.nextInt(256)));
-        g.drawLine(0,0,500,500); // Draw a line from (10,10) to (150,150)
-        for(int i = 0; i < rand.nextInt(30)+10; i++){
-            g.setColor(Color.getHSBColor(rand.nextInt(256), rand.nextInt(128)+128, rand.nextInt(256)));
-            g.fillRect(rand.nextInt(500), rand.nextInt(500), rand.nextInt(50), rand.nextInt(50));
+        g.fillRect(-10, -10, 2000, 1500);
+        //g.setColor(Color.getHSBColor(rand.nextInt(256), rand.nextInt(128)+128, rand.nextInt(256)));
+        //g.drawLine(0,0,500,500); // Draw a line from (10,10) to (150,150)
+        if(rects.isEmpty()){
+            for(int i = 0; i < rand.nextInt(100)+100; i++){
+                //g.setColor(Color.getHSBColor(rand.nextInt(256), rand.nextInt(128)+128, rand.nextInt(256)));
+                g.setColor(Color.RED);
+                Rectangle rect = new Rectangle(rand.nextInt(1680), rand.nextInt(1050), rand.nextInt(100), rand.nextInt(100));
+                g.fillRect(rect.x, rect.y, rect.width, rect.height);
+                rects.addElement(rect);
+            }
+        }
+        else
+        {
+            
+            for(int i = 0; i < rects.size(); i++){
+                Rectangle rect = rects.get(i);
+                g.setColor(Color.RED);
+                rect.x += rand.nextInt(11) - 5;
+                rect.y += rand.nextInt(11) - 5;
+                //g.setColor(Color.getHSBColor(rand.nextInt(256), rand.nextInt(128)+128, rand.nextInt(256)));
+                g.fillRect(rect.x, rect.y, rect.width, rect.height);
+            }
         }
         
     }
@@ -49,12 +68,15 @@ public class Pong extends JPanel{
     public static void main(String arg[]){
         JFrame frame = new JFrame("BasicPanel");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
+        frame.setSize(1680,1050);
         
+
         
         
         Pong panel = new Pong(frame);
         frame.setContentPane(panel);          
         frame.setVisible(true);
     }
+    
+    
 }
