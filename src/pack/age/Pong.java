c6e046bb42669de4198cc82c010f49e270491ecd
unknown <Admin@KCV-INLABDEBB69.kicvi.ldsb.edu>
2014-02-07 19:04:38 -0500

    anything

--- src/pack/age/Pong.java
+++ src/pack/age/Pong.java
@@ -17,10 +17,23 @@ public class Pong
     JFrame frame;
     public Pong()
     {
-        frame = new JFrame();
+        frame = new JFrame("PongTestText");
         frame.setSize(500, 500);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        /*JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(375, 300));
+        frame.getContentPane().add(emptyLabel , BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);*/
+        
+        Graphics rect = frame.getGraphics();
+        rect.fillRect(10, 10, 10, 10);
+        rect.setColor(Color.yellow);
+
+        //5. Show it.
+        frame.setVisible(true);
     }
 
     public static void main(String[] args)
